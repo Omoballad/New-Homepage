eab74fbd7194f0c5e808885f3fbeb2e7e9740116
Omoballad <solapeter234@gmail.com>
2025-10-25 16:41:27 +0100

    Add tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+export const content = ["./src/**/*.{js,jsx,ts,tsx,html}"];
+export const theme = {
+  extend: {
+    fontFamily: {
+      sans: ['"Open Sans"', "sans-serif"],
+    },
+  },
+};
+export const plugins = [];
